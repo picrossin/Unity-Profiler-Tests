49dcd740982306f4817a27f80ce7b0d3af953a15
Jaden Goter <jadengoter@gmail.com>
2021-12-02 23:09:40 -0600

    Pushed good and bad examples.

---
+++ Assets/Scripts/BadExampleTest.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class BadExampleTest : MonoBehaviour
+{
+    [SerializeField] private int inputSize = 20;
+
+    private void Update()
+    {
+        // Call a complicated method every frame
+        ComplicatedMethodOuter();
+    }
+    
+    private void ComplicatedMethodOuter()
+    {
+        for (int i = 0; i < inputSize; i++)
+        {
+            for (int j = 0; j < inputSize; j++)
+            {
+                Chain1();
+            }
+
+        }
+    }
+
+    private void Chain1() { Chain2(); }
+    private void Chain2() { Chain3(); }
+    private void Chain3() { Chain4(); }
+    private void Chain4() { return; }
+
+}
